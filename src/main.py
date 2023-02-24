b6b0dc0a5d845ad23d50ac5203035e2801d2ba2d
jorgerf05 <jorgefco_rf@hotmail.com>
2023-02-24 02:41:12 -0800

    first commit

--- src/main.py
+++ src/main.py
@@ -1,22 +1,28 @@
 #!/bin/python3
 
 from document import Document
-from response import Generator
+from generator import Generator
+
 
 def main():
-    
-    secciones = [
-        'Introduccion',
-        'desarrollo'
+
+    lista_secciones = [
+        'introduccion',
+        'desarrollo',
+        'conclusiones'
     ]
-    
+
+    # Inicializamos un diccionario con values None
+    dict_secciones = {section: None for section in lista_secciones}
+
     gen = Generator()
-    gen.response(secciones, "el hielo en mexico")
-    
-    #doc = Document('C:\\Users\\jorge\\Dev\\PyDoc\\src\\Introduccion.docx')
-    #doc.write('Introduccion', 'prueba de la intro')
-    #doc.write('desarrollo', 'prueba del desarrollo')
-    #doc.build()
-    
-    
-if __name__ == "__main__":main()
+    gen.fillDocument(dict_secciones, "Sensores para arduino UNO")
+
+    # TODO: Parsear esta ruta como argumentos del script
+    doc = Document('C:\\Users\\jorge\\OneDrive\\Documentos\\template_sistemas_programables.docx')
+    doc.write(dict_secciones)
+    doc.build()  # Guardamos el documento
+
+
+if __name__ == "__main__":
+    main()
